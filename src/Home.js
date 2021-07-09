49e6e29f9e1830f0bac21cba6d61fc60fff883fc
hamza mugabo <hamzamugabo3@gmail.com>
2021-07-09 18:24:18 +0300

    add to cart

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,56 @@
 import * as React from 'react';
-import { Text, View,FlatList,ActivityIndicator ,StyleSheet,TouchableHighlight,ScrollView,Image,TouchableOpacity} from 'react-native';
-// import database from '@react-native-firebase/database';
-// import storage from '@react-native-firebase/storage';
+import {
+  Text,
+  View,
+  FlatList,
+  ActivityIndicator,
+  StyleSheet,
+  TouchableHighlight,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+// import { Avatar, Badge, Icon, withBadge } from 'react-native-elements'
 import call from 'react-native-phone-call';
-import Card from "./Card"
+import Card from './Card';
 import AsyncStorage from '@react-native-community/async-storage';
-import { ListItem, SearchBar } from "react-native-elements";
-
-export default class Home extends React.Component{
+import Dialog, {
+  DialogFooter,
+  DialogButton,
+  DialogContent,
+} from 'react-native-popup-dialog';
+import {
+  ListItem,
+  SearchBar,
+  Avatar,
+  Badge,
+  Icon,
+  withBadge,
+} from 'react-native-elements';
+var cart = [];
+export default class Home extends React.Component {
   constructor() {
     super();
 
     this.state = {
-     
-      data:[],
-      data2:[],
-      is_admin:false,
+      products: [],
+      data: [],
+      data2: [],
+      is_admin: false,
       currentUser: null,
       confirm_agreement: false,
       loading: false,
       disabled: false,
-      photos:[],
-      photos2:[],
-      value:'',
-      setland:false,
-      setbuilding:false
+      visible: false,
+      photos: [],
+      photos2: [],
+      value: '',
+      setland: false,
+      setbuilding: false,
     };
   }
-  all=()=>{
-    this.setState({loading:true,value:''});
+  all = () => {
+    this.setState({loading: true, value: ''});
     fetch(
       // 192.168.42.153
       'https://ubuntusx.com/mobipharm/products.php',
@@ -36,22 +58,21 @@ export default class Home extends React.Component{
     )
       .then(response => response.json())
       .then(json => {
-        this.setState({data:json});
+        this.setState({data: json});
         // setData(json);
         // setisLoading(false);
-    this.setState({loading:false});
-
+        this.setState({loading: false});
       })
       .catch(error => {
         if (error) {
           console.error(error);
-          this.setState({loading:false})
+          this.setState({loading: false});
         }
       })
-      .finally(() => this.setState({loading:false}));
-  }
-  componentDidMount(){
-    this.setState({loading:true});
+      .finally(() => this.setState({loading: false}));
+  };
+  componentDidMount() {
+    this.setState({loading: true});
     fetch(
       // 192.168.42.153
       'https://ubuntusx.com/mobipharm/products.php',
@@ -59,62 +80,67 @@ export default class Home extends React.Component{
     )
       .then(response => response.json())
       .then(json => {
-        this.setState({data:json});
+        this.setState({data: json});
         // setData(json);
         // setisLoading(false);
-    this.setState({loading:false});
-
+        this.setState({loading: false});
       })
       .catch(error => {
         if (error) {
           console.error(error);
-          this.setState({loading:false})
+          this.setState({loading: false});
         }
       })
-      .finally(() => this.setState({loading:false}));
+      .finally(() => this.setState({loading: false}));
   }
-  call = (item) => {
+  call = item => {
     const args = {
       number: item.number,
       prompt: false,
     };
     call(args).catch(console.error);
   };
-  makeLoanpayments =async (data)=>{
+  makeLoanpayments = async data => {
     // console.log(data);
     try {
-      const jsonValue = JSON.stringify(data)
-      await AsyncStorage.setItem('@payment', jsonValue)
-// alert(e);
-this.props.navigation.navigate('MakePayments');
-// setScaleAnimationDialogRepayLoan(false);
+      const jsonValue = JSON.stringify(data);
+      await AsyncStorage.setItem('@payment', jsonValue);
+      // alert(e);
+      this.props.navigation.navigate('MakePayments');
+      // setScaleAnimationDialogRepayLoan(false);
     } catch (e) {
       // saving error
-alert(e);
+      alert(e);
     }
     // navigation.navigate('MakePayments');
     // setScaleAnimationDialogRepayLoan(false);
 
-// console.log(data);
-  }
+    // console.log(data);
+  };
+  buy = item => {
+    cart.push(item.id);
+    var unique = cart.filter((v, i, a) => a.indexOf(v) === i);
+    console.log(unique);
+    this.setState({products: unique});
+  };
   renderSeparator = () => {
     return (
       <View
         style={{
           height: 1,
-          width: "86%",
-          backgroundColor: "#CED0CE",
+          width: '86%',
+          backgroundColor: '#CED0CE',
         }}
       />
     );
   };
 
-  searchFilterFunction = (text) => {
+  searchFilterFunction = text => {
     this.setState({
       value: text,
     });
 
-    const newData = this.state.data.filter((item) => {
+    const newData = this.state.data.filter(item => {
       const itemData = `${item.item.toUpperCase()} ${item.category.toUpperCase()}${item.price.toUpperCase()}`;
       const textData = text.toUpperCase();
 
@@ -131,133 +157,155 @@ alert(e);
         placeholder="Search..."
         lightTheme
         round
-        onChangeText={(text) => this.searchFilterFunction(text)}
+        onChangeText={text => this.searchFilterFunction(text)}
         autoCorrect={false}
         value={this.state.value}
       />
     );
   };
-  _renderItem = ({ item }) => {
-   
+  _renderItem = ({item}) => {
     return (
-      
-      <Card
-      style={{width:'100%'}}
-      >
-        {
-          this.state.loading?
-          <ActivityIndicator size="large" animating={true} color='#bc2b78' />
-        :
-        <View>
-<View style={[styles.buttonsContainers, {marginBottom: 20}]}>
+      <Card style={{width: '100%'}}>
+        {this.state.loading ? (
+          <ActivityIndicator size="large" animating={true} color="#bc2b78" />
+        ) : (
+          <View style={styles.buttonsContainers}>
+            <View style={{marginBottom: 20}}>
+              <Image
+                style={{width: 70, height: 80, borderRadius: 50}}
+                source={{
+                  uri: `http://ubuntusx.com/mobipharm/uploads/${item.image}`,
+                }}
+              />
+            </View>
+            <View style={{marginLeft: 40}}>
+              <View style={[styles.buttonsContainers, {marginBottom: 20}]}>
+                <Text style={styles.details}>category:</Text>
+                <Text style={styles.value}>{item.category}</Text>
+              </View>
+              <View style={[styles.buttonsContainers, {marginBottom: 20}]}>
+                <Text style={styles.details}>product:</Text>
+                <Text style={styles.value}>{item.item}</Text>
+              </View>
 
+              <View style={[styles.buttonsContainers, {marginBottom: 20}]}>
+                <Text style={styles.details}>price:</Text>
+                <Text style={styles.value}>{item.price}</Text>
+              </View>
 
-<Image
-style={{width: '100%', height: 80}}
-source={{uri: `http://ubuntusx.com/mobipharm/uploads/${item.image}`}}
-/>
+              {/* <View style={[styles.buttonsContainers, {marginBottom: 20}]}>
+                <Text style={styles.details}>quantity:</Text>
+                <Text style={styles.value}>{item.quantity}</Text>
+              </View> */}
 
-</View>
-<View style={[styles.buttonsContainers, {marginBottom: 20}]}>
-  <Text style={styles.details}>category:</Text>
-  <Text style={styles.value}>{item.category}</Text>
-</View>
-<View style={[styles.buttonsContainers, {marginBottom: 20}]}>
-  <Text style={styles.details}>product:</Text>
-  <Text style={styles.value}>{item.item}</Text>
-</View>
+              {/* <View style={[styles.buttonsContainers, {marginBottom: 20}]}>
+                <Text style={styles.details}>Description:</Text>
+                <Text style={styles.value}>{item.discription}</Text>
+              </View> */}
+            </View>
+            <View style={{marginLeft: 40}}>
+              <TouchableOpacity
+                onPress={this.buy.bind(this, item)}
+                style={{
+                  height: 45,
+                  flexDirection: 'row',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  marginBottom: 20,
+                  // width: 250,
+                  backgroundColor: '#ff751a',
+                  width: 60,
+                  borderRadius: 10,
+                }}>
+                <Text style={{color: '#fff'}}>Buy</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        )}
 
-<View style={[styles.buttonsContainers, {marginBottom: 20}]}>
-  <Text style={styles.details}>price:</Text>
-  <Text style={styles.value}>{item.price}</Text>
-</View>
-
-<View style={[styles.buttonsContainers, {marginBottom: 20}]}>
-  <Text style={styles.details}>quantity:</Text>
-  <Text style={styles.value}>{item.quantity}</Text>
-</View>
-
-<View style={[styles.buttonsContainers, {marginBottom: 20}]}>
-  <Text style={styles.details}>Description:</Text>
-  <Text style={styles.value}>{item.discription}</Text>
-</View>
-
-        </View>
-
-
-        }
-      
-      {/* </View> */}
+        {/* </View> */}
       </Card>
     );
   };
-  render(){
+  render() {
     return (
-      <View style={{ flex: 1,marginTop:10}}>
-         <View style={[styles.buttonsContainer, {margin:15}]}>
-<TouchableHighlight>
-  <Text style={{fontSize:25}}>Mobipharm</Text>
-</TouchableHighlight>
-<TouchableOpacity
-onPress={() => this.props.navigation.navigate('Settings')}
->
-  <Image source={require('./images/settings.png')}
-   style = {{ width: 30, height: 30 }}
-  />
-  {/* <Text>Settings</Text> */}
+      <View style={{flex: 1, marginTop: 10}}>
+        <View style={[styles.buttonsContainer, {margin: 15}]}>
+          <TouchableHighlight>
+            <Text style={{fontSize: 25}}>Mobipharm</Text>
+          </TouchableHighlight>
+          <TouchableOpacity
+            onPress={() => this.props.navigation.navigate('Settings')}>
+            <Image
+              source={require('./images/settings.png')}
+              style={{width: 30, height: 30}}
+            />
+            {/* <Text>Settings</Text> */}
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => this.setState({visible: true})}>
+            <View>
+              <Badge
+                value={this.state.products.length}
+                status="warning"
+                containerStyle={{right: -10, top: 2}}
+              />
 
-</TouchableOpacity>
-         </View>
-         <View style={[styles.buttonsContainer, {margin:15}]}>
-         <View  style={{borderBottomColor:'orange',padding:10}}>
-        <TouchableHighlight style={[styles.buttonContainer,{height:30}]}><Text>SORT BY</Text></TouchableHighlight>
-                 
-                </View>
-                <View  style={{borderBottomColor:'orange',padding:10}}>
-                 <TouchableHighlight
-                 onPress={this.all}
-                style={[styles.buttonContainer, styles.loginButton,{height:30}]}
-                // onPress={this.searchFilterFunction}
-                
-                // onPress={this.all}
+              <Image
+                style={{width: 40, height: 40, borderRadius: 50}}
+                source={require('./images/cart1.jpg')}
+              />
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View style={[styles.buttonsContainer, {margin: 15}]}>
+          <View style={{borderBottomColor: 'orange', padding: 10}}>
+            <TouchableHighlight style={[styles.buttonContainer, {height: 30}]}>
+              <Text>SORT BY</Text>
+            </TouchableHighlight>
+          </View>
+          <View style={{borderBottomColor: 'orange', padding: 10}}>
+            <TouchableHighlight
+              onPress={this.all}
+              style={[styles.buttonContainer, styles.loginButton, {height: 30}]}
+              // onPress={this.searchFilterFunction}
 
+              // onPress={this.all}
+            >
+              <Text style={styles.loginText}>All</Text>
+            </TouchableHighlight>
+          </View>
+          <View style={{borderBottomColor: 'orange', padding: 10}}>
+            <TouchableHighlight
+              onPress={() => this.setState({setland: true, setbuilding: false})}
+              style={[
+                styles.buttonContainer,
+                styles.loginButton,
+                {width: 90, height: 30},
+              ]}
+              // onPress={this.searchFilterFunction}
 
-                >
-                <Text style={styles.loginText}>All</Text>
-              </TouchableHighlight>
-                </View>
-        <View  style={{borderBottomColor:'orange',padding:10}}>
-                 <TouchableHighlight
-                 onPress={()=>this.setState({setland:true,setbuilding:false})}
-                style={[styles.buttonContainer, styles.loginButton,{width:90,height:30}]}
-                // onPress={this.searchFilterFunction}
-                
-                // onPress={this.all}
-
-
-                >
-                <Text style={styles.loginText}>Category</Text>
-              </TouchableHighlight>
-                </View>
-                
-                </View>
-                <View style={{marginBottom:200}}>
-                  {this.state.loading?
-          <ActivityIndicator size="large" animating={true} color='#bc2b78' />
-                  :
-                  <FlatList
-          data={this.state.data}
-          renderItem={this._renderItem}
-          keyExtractor={(item, index) => {
-            return index.toString();
-          }}
-          ItemSeparatorComponent={this.renderSeparator}
-          ListHeaderComponent={this.renderHeader}
-        />
-                }
-                
-                </View>
-                {/* <FlatList
+              // onPress={this.all}
+            >
+              <Text style={styles.loginText}>Category</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+        <View style={{marginBottom: 200}}>
+          {this.state.loading ? (
+            <ActivityIndicator size="large" animating={true} color="#bc2b78" />
+          ) : (
+            <FlatList
+              data={this.state.data}
+              renderItem={this._renderItem}
+              keyExtractor={(item, index) => {
+                return index.toString();
+              }}
+              ItemSeparatorComponent={this.renderSeparator}
+              ListHeaderComponent={this.renderHeader}
+            />
+          )}
+        </View>
+        {/* <FlatList
               data={this.state.data}
               renderItem={this._renderItem}
               keyExtractor={(item, index) => {
@@ -266,15 +314,51 @@ onPress={() => this.props.navigation.navigate('Settings')}
               ItemSeparatorComponent={this.renderSeparator}
               // ListHeaderComponent={this.header}
             /> */}
+
+        <Dialog visible={this.state.visible}
+        
+        footer={
+          <DialogFooter>
+            <DialogButton
+              text="CANCEL"
+              onPress={() =>this.setState({visible:false})}
+            />
+            <DialogButton
+              text="OK"
+              onPress={() =>this.setState({visible:false})}
+            />
+          </DialogFooter>
+        }
+        >
+          <DialogContent>
+            <View
+              style={{
+                margin: 70,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              {this.state.products.map(product_id =>
+                this.state.data
+                  .filter(item => item.id == product_id)
+                  .map((product, index) => (
+                    <View key={index}>
+                      <Text>{product.item}</Text>
+                      <Text>{product.category}</Text>
+                      <Text>{product.price}</Text>
+                    </View>
+                  )),
+              )}
+            </View>
+          </DialogContent>
+        </Dialog>
       </View>
-     
     );
   }
 }
 const styles = StyleSheet.create({
   buttonsContainer: {
     flexDirection: 'row',
-    width: "90%",
+    width: '90%',
     justifyContent: 'space-between',
     paddingHorizontal: 0,
     alignItems: 'center',
@@ -299,7 +383,7 @@ const styles = StyleSheet.create({
   },
   loginButton: {
     backgroundColor: '#000080',
-    padding:7
+    padding: 7,
   },
   loginText: {
     color: '#58f406',
